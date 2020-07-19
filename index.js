ff83a9f74a46588ac54eef5b5691632e859a4429
TAKZOBYE <takerchanel@gmail.com>
2020-07-19 10:05:19 +0700

    Upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function appendImageElement(keyword, index) {
+    const imageElement = document.createElement('img');
+    imageElement.src = `https://source.unsplash.com/400x225/?${keyword}&sig=${index}`;
+
+    const galleryElement = document.querySelector('.gallery');
+    galleryElement.appendChild(imageElement);
+}
+
+function removePhoto() {
+    const galleryElement = document.querySelector('.gallery');
+    galleryElement.innerHTML ='';
+}
+
+function searchPhoto(event) {
+    const keyword = event.target.value;
+    removePhoto()
+    if(event.key === 'Enter' && keyword) {
+        for (let i = 1; i <= 9; i++) {
+            appendImageElement(keyword, i)
+        }
+    }
+    console.log(keyword);
+}
+
+function run() {
+    const inputElement = document.querySelector('input');
+    inputElement.addEventListener('keydown', searchPhoto);
+}
+
+run();
